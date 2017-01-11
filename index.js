395b25c079223d8f0889788e870a17f1fd5cc9a1
yutaka_houlette <yutakahoulette@gmail.com>
2017-01-11 14:28:28 -0800

    Add height-setting logic

--- index.js
+++ index.js
@@ -3,35 +3,56 @@ import flyd from 'flyd'
 import h from 'snabbdom/h'
 
 
-module.exports = state => {
-  return h('div'
-  , {
-      attrs: {'ff-toggle-box': ''}
-    }
-  , [
-    h('div'
-    , {
-        attrs: {'ff-toggle-box-top': ''}
-      , on: {click: toggleID(state)} 
-      }
-    , [state.top] || ''
-    )
-  , h('div'
-    , {
-        attrs: {'ff-toggle-box-bottom': ''}
-      , class: {hide: !state.parentState.toggleBoxIDs$()[state.thisID]}
-      }
-    , [state.bottom] || ''
-    )
-  ])
-}
-
 const toggleID = state => ev => {
   const IDs = state.parentState.toggleBoxIDs$()
   const thisID = state.thisID
-
   state.parentState.toggleBoxIDs$(
     R.assoc(thisID, IDs[thisID] ? false : true, IDs)
   )
 }
+
+
+const setHeight = state => node => {
+  const isOpen = state.parentState.toggleBoxIDs$()[state.thisID]
+  const elm = node.elm
+  const height = elm.querySelector('[ff-toggle-box-bottom]').offsetHeight
+  if(isOpen) {
+    elm.style.height = height + 'px'
+  } else {
+    elm.style.height = 0 
+  }
+}
+
+
+const top = state =>
+  h('div', { attrs: {'ff-toggle-box-top': ''},  on: {click: toggleID(state)}}
+  , [state.top] || '')
+
+
+const bottom = state => 
+ h('div'
+  , {attrs: {'ff-toggle-box-bottom-wrapper': ''}
+    , hook: {
+        update: setHeight(state)
+      , insert: vnode => {
+          window.addEventListener('resize', ev => setHeight(state)(vnode)) 
+        }
+    }}
+  , [ h('div', {attrs: {'ff-toggle-box-bottom': ''}}, [state.bottom || '']) ])
+
+
+module.exports = state => {
+  return h('div'
+  , {
+      attrs: {'ff-toggle-box': state.parentState.toggleBoxIDs$()[state.thisID]
+        ? 'is-open'
+        : 'is-closed'
+      }
+    }
+  , [
+    top(state)
+  , bottom(state)
+  ])
+}
+
 
