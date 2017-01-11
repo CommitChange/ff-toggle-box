72e4e606d6db704bc2d2cab71dd089c176c193ff
yutaka_houlette <yutakahoulette@gmail.com>
2017-01-10 23:18:03 -0800

    Add basica toggling functionality

--- index.js
+++ index.js
@@ -2,3 +2,37 @@ import R from 'ramda'
 import flyd from 'flyd'
 import h from 'snabbdom/h'
 
+
+module.exports = state => {
+  return h('div'
+  , {
+      attrs: {'ff-toggle-box': ''}
+    , class: {yes: 'asdf'}
+    }
+  , [
+    h('div'
+    , {
+        attrs: {'ff-toggle-box-top': ''}
+      , on: {click: toggleID(state)} 
+      }
+    , [state.top] || ''
+    )
+  , h('div'
+    , {
+        attrs: {'ff-toggle-box-bottom': ''}
+      , class: {hide: !state.parentState.toggleBoxIDs$()[state.thisID]}
+      }
+    , [state.bottom] || ''
+    )
+  ])
+}
+
+const toggleID = state => ev => {
+  const IDs = state.parentState.toggleBoxIDs$()
+  const thisID = state.thisID
+
+  state.parentState.toggleBoxIDs$(
+    R.assoc(thisID, IDs[thisID] ? false : true, IDs)
+  )
+}
+
--- page.js
+++ page.js
@@ -2,31 +2,42 @@ import R from 'ramda'
 import flyd from 'flyd'
 import snabbdom from 'snabbdom'
 import h from 'snabbdom/h'
-import render from 'ff-core/render'
+import render from 'flimflam-render'
+
+import toggleBox from './index.js'
 
 function init() {
-  let state = {}
+  let state = {
+    toggleBoxIDs$ : flyd.stream({})
+  }
+
+  flyd.map(x => console.log(x), state.toggleBoxIDs$)
 
   return state
 }
 
 function view(state) {
   return h('div', [
-    'test test test'
+    toggleBox({ 
+      thisID: 'toggleBox'
+    , parentState: state
+    , top: h('h1', 'top') 
+    , bottom: h('h1', 'bottom') 
+    })
   ])
 }
 
 const patch = snabbdom.init([
-  require("snabbdom/modules/class")
-, require("snabbdom/modules/style")
-, require("snabbdom/modules/props")
-, require("snabbdom/modules/eventlisteners")
-, require("snabbdom/modules/attributes")
+  require("snabbdom/modules/class").default
+, require("snabbdom/modules/style").default
+, require("snabbdom/modules/props").default
+, require("snabbdom/modules/eventlisteners").default
+, require("snabbdom/modules/attributes").default
 ])
 
 const container = document.querySelector('#container')
 
 const state = init()
 
-render({container, state, view, patch})
+render({patch, container, view, state})
 
